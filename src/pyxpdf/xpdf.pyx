dab0369ef20127d82cc309f478a83b7286010ecc
Ashutosh Varma <ashutoshvarma11@live.com>
2020-04-13 18:03:33 +0530

    refactor: move all error callback handling from xpdf.pyx to pdferror.pxi

--- src/pyxpdf/xpdf.pyx
+++ src/pyxpdf/xpdf.pyx
@@ -12,34 +12,6 @@ include "helper.pxi"
 
 include "pdferror.pxi"
 
-from pyxpdf.includes.xpdf_error cimport ErrorCategory, setErrorCallback, ErrorCallback
-# Dummy callback to silence errors for now.
-cdef int xpdf_error_callback(void *data, ErrorCategory category, int pos, char *msg) except -1:
-    message = msg.decode('UTF-8') 
-    if pos != -1:
-        message += " , at internal position {pos}".format(pos=pos)
-
-    if category == ErrorCategory.errSyntaxError:
-        raise PDFSyntaxError(message)
-    elif category == ErrorCategory.errConfig:
-        raise XPDFConfigError(message)
-    elif category == ErrorCategory.errIO:
-        raise PDFIOError(message)
-    elif category == ErrorCategory.errNotAllowed:
-        raise PDFPermissionError(message)
-    elif category == ErrorCategory.errUnimplemented:
-        raise XPDFNotInplementedError(message)
-    elif category == ErrorCategory.errInternal:
-        raise XPDFInternalError(message)
-    elif category == ErrorCategory.errSyntaxWarning:
-        #TODO: Handle warnings
-        pass
-    else:
-        raise XPDFError("Unkown Error Occured")
-
-
-setErrorCallback(<ErrorCallback>&dummpy_error_callback, NULL)
-
 
 from os import linesep
 
