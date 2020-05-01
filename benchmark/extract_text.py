47460ceb4ece8f7a1d14441fe4d6e9a4153e4312
Ashutosh Varma <ashutoshvarma11@live.com>
2020-05-01 22:33:51 +0530

    benchmark: fix relative path for samples pdf

--- benchmark/extract_text.py
+++ benchmark/extract_text.py
@@ -1,14 +1,21 @@
 # coding=utf-8
 
+import os
 from time import time
 from pyxpdf import Document
 
+BENCH_DIR = os.path.dirname(os.path.realpath(__file__))
+ROOT_DIR = os.path.dirname(BENCH_DIR)
+SMAPLES_DIR = os.path.join(os.path.join(ROOT_DIR, 'samples'))
+
+mandarin_pdf = os.path.join(SMAPLES_DIR, 'nonfree', 'mandarin.pdf')
+dmca_pdf = os.path.join(SMAPLES_DIR, 'nonfree', 'dmca.pdf')
 
 def run():
     t0 = time()
 
-    text1 = Document("samples/nonfree/mandarin.pdf").text()
-    text2 = Document("samples/nonfree/dmca.pdf").text()
+    text1 = Document(mandarin_pdf).text()
+    text2 = Document(dmca_pdf).text()
 
     tk = time()
     return tk - t0
